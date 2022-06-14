1eb88189eff33b91b9358c93f1b005874e068ae3
xycxycxyc0 <98386834+xycxycxyc0@users.noreply.github.com>
2022-06-14 14:05:05 +0800

    Add files via upload

---
+++ java002/src/com/Calender.java
@@ -0,0 +1,349 @@
+package com;
+
+import java.awt.BorderLayout;
+
+import java.awt.Color;
+
+import java.awt.Font;
+
+import java.awt.GridLayout;
+
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import java.util.GregorianCalendar;
+import javax.swing.JButton;
+
+import javax.swing.JComboBox;
+
+import javax.swing.JFrame;
+
+import javax.swing.JLabel;
+
+import javax.swing.JPanel;
+
+/*日历程序*/
+
+public class Calender extends JFrame implements ActionListener,Runnable {
+
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// 月份和年份的下拉列表框
+
+	JComboBox MonthBox = new JComboBox();
+	JComboBox YearBox = new JComboBox();
+
+	// 月份和年份的标签
+	JLabel shuru = new JLabel("请输入日期");
+	JLabel YearLabel = new JLabel("年份: ");
+	JLabel MonthLabel = new JLabel("月份: ");
+
+	// “确定”、“今天”、"上一月"、"下一月"四个按钮
+
+    JButton button_ok = new JButton("确定");
+	JButton button_today = new JButton("今天");
+	JButton button_upmonth = new JButton("上月");
+	JButton button_downmonth = new JButton("下月");
+	
+	
+	// 获取今天的日期、年份、月份
+
+	Date now_date = new Date();
+	int now_year = now_date.getYear() + 1900;
+	int now_month = now_date.getMonth();
+
+	// 用否显示今天的日期
+
+	boolean todayFlag = false;
+
+	// 用一组按钮显示日期，一共7行7列，第一行为星期的名字
+
+	JButton[] button_day = new JButton[42];
+	String[] week = { "日", "一", "二", "三", "四", "五", "六" };
+	JButton[] button_week = new JButton[7];
+
+	// 保存用户选择的年份
+
+	String year_int = null;
+
+	// 保存用户选择的月份
+
+	int month_int;
+	//时间
+	JLabel Now_Time = new JLabel();
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 hh时mm分ss秒");
+	 
+
+/**
+
+* 构造函数
+
+*/
+
+	public Calender() {
+		super();
+		this.setTitle("日 历");
+		this.init();
+		this.setLocation(400, 300);
+		pack();
+	}
+	
+/**
+
+* 初始化日历
+
+*/
+
+	private void init() {
+
+		Font font = new Font("Dialog", Font.BOLD, 14);
+		shuru.setFont(font);
+		YearLabel.setFont(font);
+		MonthLabel.setFont(font);
+		button_ok.setFont(font);
+		button_today.setFont(font);
+
+		// 设定年份区间，为当前年份的过去2021年到当前年份的未来5000年，
+
+		for (int i = now_year - 2022; i <= now_year + 5000; i++) {
+			YearBox.addItem(i + "");
+		}
+
+		// 设定年份下拉列表为当前年份，当前月份处于第十项
+		YearBox.setSelectedIndex(2022);
+
+		// 设定月份区间，12个月
+		for (int i = 1; i < 13; i++) {
+			MonthBox.addItem(i + "");
+		}
+
+		// 设定月份下拉列表为当前月份
+
+		MonthBox.setSelectedIndex(now_month);
+
+		// 放置下拉列表框和控制按钮的面板
+
+		JPanel panel_ym = new JPanel();
+		panel_ym.add(shuru);
+		panel_ym.add(YearLabel);
+		panel_ym.add(YearBox);
+		panel_ym.add(MonthLabel);
+		panel_ym.add(MonthBox);
+		panel_ym.add(button_ok);
+		panel_ym.add(button_today);
+		panel_ym.add(button_upmonth);
+		panel_ym.add(button_downmonth);
+		
+		Now_Time = new JLabel(sdf.format(new Date()));
+		//赋予JLabel一些属性值。
+		Now_Time.setForeground(Color.DARK_GRAY);
+		Now_Time.setFont(new Font("微软雅黑", Font.PLAIN, 12));
+		Now_Time.setBounds(233, 36, 250, 15);
+		panel_ym.add(Now_Time);
+
+		// 为4个按钮添加事件侦听器
+
+		button_ok.addActionListener(this);
+		button_today.addActionListener(this);
+		button_upmonth.addActionListener(this);
+		button_downmonth.addActionListener(this);
+
+		// 放置日期面板
+
+		JPanel panel_day = new JPanel();
+
+		// 网格布局管理器，7行7列，网格之间水平和垂直方向上间隔均为5
+
+		panel_day.setLayout(new GridLayout(7, 7, 3, 3));
+
+		// 添加星期的名字，并放到画板里
+
+		for (int i = 0; i < 7; i++) {
+
+			button_week[i] = new JButton(" ");
+			button_week[i].setText(week[i]);
+			button_week[0].setForeground(Color.black);
+			panel_day.add(button_week[i]);
+		}
+
+		button_week[0].setForeground(Color.red);
+		button_week[6].setForeground(Color.red);
+
+		// 添加日期，放到画板里
+
+		for (int i = 0; i < 42; i++) {
+			button_day[i] = new JButton(" ");
+			panel_day.add(button_day[i]);
+		}
+
+		// 显示当前年月的日期
+		this.paintDay();
+		
+
+		// 放置以上两个面板
+		JPanel panel_main = new JPanel();
+		// 边界布局管理器
+
+		panel_main.setLayout(new BorderLayout());
+		panel_main.add(panel_ym, BorderLayout.NORTH);		
+		panel_main.add(panel_day, BorderLayout.SOUTH);
+		getContentPane().add(panel_main);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
+/**
+
+* 显示当前年月的日期
+
+*/
+
+	private void paintDay() {
+		if (todayFlag) {
+			// 要求显示今天的日历
+			year_int = now_year + "";
+			month_int = now_month;
+		} else {
+			// 否则，从下拉框中获取用户选择的年月
+			year_int = YearBox.getSelectedItem().toString();
+			month_int = MonthBox.getSelectedIndex();// 被选的序号
+		}
+		// 获得年份值
+		int year_sel = Integer.parseInt(year_int) - 1900;
+		// 构造该月的第一天
+		Date firstDay = new Date(year_sel, month_int, 1);
+		// 创建一个Calendar实例
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.setTime(firstDay);
+		
+
+		int days = 0; // 存放某个月份的天数
+		int day_week = 0; // 存放某个月的第一天是星期几的数值
+		int day_week2 = 0;
+
+		// 判断是几月份,根据它来设定day的值 其中二月份要判断是否是闰年
+		if (month_int == 0 || month_int == 2 || month_int == 4 || month_int == 6 || month_int == 7 || month_int == 9 || month_int == 11) {
+			days = 31;
+		} else if (month_int == 3 || month_int == 5 || month_int == 8 || month_int == 10) {
+			days = 30;
+		} else {
+			// 二月，如果是闰年，则有29天，否则有28天
+			if (cal.isLeapYear(year_sel)) {
+				days = 29;
+			} else {
+				days = 28;
+			}
+		}
+		// getDay方法获取该天是星期几
+		day_week = firstDay.getDay();
+		int count = 1;
+
+/*
+*绘制按钮。在这里首先要根据选定的月份的第一天是星期几来确定绘制按钮的起始位置
+*其中day_week就是我们要绘制的起始位置，对于那些没有数值可以显示的按钮要换颜色显示。
+*/
+
+		for (int i = day_week; i < day_week + days; count++, i++) {
+			if (i % 7 == 0 || i == 6 || i == 13 || i == 20 || i == 27 || i == 34 || i == 41) {
+				// 如果是边界上的按钮，文字用红色，以标示周末
+				if (i == day_week + now_date.getDate() - 1) {
+					// 将跟今天一样的日期用蓝色标示
+					button_day[i].setForeground(Color.blue);
+					button_day[i].setText(count + "");
+				} else {
+					// 其他边界上的按钮中的文字用红色
+					button_day[i].setForeground(Color.red);
+					button_day[i].setText(count + "");
+				}
+			} else {
+				if (i == day_week + now_date.getDate() - 1) {
+					button_day[i].setForeground(Color.blue);
+					button_day[i].setText(count + "");
+				} else {
+					// 一般位置的按钮上的文字用黑色标示
+					button_day[i].setForeground(Color.black);
+					button_day[i].setText(count + "");
+				}
+			}
+		}
+		// 对于没有日期数值显示的按钮进行处理
+		int After_Day = 1;
+        cal.set(cal.MONTH, cal.get(cal.MONTH)-1);
+        int lastDateOfPrevMonth = cal.getActualMaximum(cal.DAY_OF_MONTH);
+        int nowMonthfirstButton = lastDateOfPrevMonth - day_week + 1;
+		
+		if (day_week == 0) {
+			// 如果第一天是周日，则将后面的按钮上的文字都设为下月的日期
+			for (int i = days; i < 42; i++, After_Day++) {
+				button_day[i].setForeground(Color.gray);
+				button_day[i].setText(After_Day + "");
+			}
+		} else {
+			// 如果第一天不是周日，则将第一天前面的按钮置空
+			for (int i = 0; i < day_week; i++, nowMonthfirstButton++) {
+				button_day[i].setForeground(Color.gray);
+				button_day[i].setText(nowMonthfirstButton + "");
+			} // 最后一天后面的按钮设为下月的日期
+			for (int i = day_week + days; i < 42; i++, After_Day++) {
+				button_day[i].setForeground(Color.gray);
+				button_day[i].setText(After_Day + "");
+			}
+		}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == button_ok) {
+			// 如果点击确定按钮就调用setDay()重新方法绘制按钮
+			todayFlag = false;
+			this.paintDay();
+		} else if (e.getSource() == button_today) {
+			// 如果点击今天按钮，得到今天的日期
+			todayFlag = true;
+			YearBox.setSelectedIndex(2022);
+			MonthBox.setSelectedIndex(now_month);
+			this.paintDay();
+		} else if (e.getSource() == button_upmonth){//点击上月可得到上月日期
+			 todayFlag = false;
+			 if (month_int == 0) {
+				YearBox.setSelectedIndex(YearBox.getSelectedIndex() - 1);
+				MonthBox.setSelectedIndex(11);
+			}else {
+				MonthBox.setSelectedIndex(MonthBox.getSelectedIndex() - 1);
+			}
+			 this.paintDay(); 
+		} else if (e.getSource() == button_downmonth){//点击下月可得到下月日期
+			 todayFlag = false;
+			 if (month_int == 11) {
+				YearBox.setSelectedIndex(YearBox.getSelectedIndex() + 1);
+				MonthBox.setSelectedIndex(0);
+			}else {
+				MonthBox.setSelectedIndex(MonthBox.getSelectedIndex() + 1);
+			}
+			 this.paintDay(); 
+		}
+	}
+	
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(1000);//Thread.Sleep()方法用于将当前线程休眠一定时间，单位为毫秒。这里为每1000毫秒休眠一次线程。
+				Now_Time.setText(sdf.format(new Date()));//把当前的系统时间赋予到我们自定义的JLabel中
+			} catch (InterruptedException e) {
+				e.printStackTrace();//抛出异常
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Calender ct = new Calender();
+		new Thread(ct).start();//启动线程
+	}
+
+}
+
---
+++ java002/src/com/NotePad.java
@@ -0,0 +1,434 @@
+package com;
+
+import java.awt.BorderLayout;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class NotePad extends JFrame implements ActionListener {  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+//创建NotePadWinddow类用来建立记事本GUI
+    //定义窗体组件
+	//定义菜栏条组件
+	JMenuBar jmb;
+	//定义菜单组件
+	JMenu []jm=new JMenu[5];
+	//定义菜单项
+	JMenuItem []jmi=new JMenuItem[23];
+	//定义文本域组件
+	JTextArea jta;
+	//定义滚动窗格组件
+	JScrollPane jsp;
+	//声明变量，作用是判断文本变化是否有保存
+	int flag = 0;
+
+	//定义成员变量文件对象，用于保存打开的文件对象
+	File selectedFile;
+	//如果"打开"操作，会将打开的文件路径覆盖此路径；如果此路径为空，可以保存为新文件；
+	//创建文件选择器对象，用于展示打开和保存界面，从而选择文件
+	JFileChooser chooser = new JFileChooser();
+	
+	//退出提示框
+	JFrame alertFrame = new JFrame();
+	JButton saveButton = new JButton("保存");
+	JButton notsaveButton = new JButton("不保存");
+	JButton cancelButton = new JButton("取消");
+	
+	public NotePad() {
+		// TODO Auto-generated constructor stub
+		init();
+	}
+	
+	public void init() { 
+		//实现组件
+		//实现菜单条组件
+		jmb=new JMenuBar();
+		//实现菜单组件
+		jm[0] = new JMenu("文件(F)");
+		jm[1] = new JMenu("编辑(E)");
+		jm[2] = new JMenu("格式(O)");
+		jm[3] = new JMenu("查看(V)");
+		jm[4] = new JMenu("帮助(H)");
+		//实现文件(F)菜单的菜单项组件
+		jmi[0] = new JMenuItem("新建(N)");
+		jmi[0].addActionListener(this);
+		jmi[1] = new JMenuItem("打开(O)");
+		jmi[1].addActionListener(this);
+		jmi[2] = new JMenuItem("保存(S)");
+		jmi[2].addActionListener(this);
+		jmi[3] = new JMenuItem("另存为(A)");
+		jmi[3].addActionListener(this);
+		jmi[4] = new JMenuItem("页面设置(U)");
+		jmi[5] = new JMenuItem("打印(P)");
+		jmi[6] = new JMenuItem("退出(X)");
+		jmi[6].addActionListener(this);
+		//实现编辑(E)菜单的菜单项组件
+		jmi[7] = new JMenuItem("撤销(Z)");
+		jmi[8] = new JMenuItem("剪切(X)");
+		jmi[9] = new JMenuItem("复制(C)");
+		jmi[10] = new JMenuItem("粘贴(P)");
+		jmi[11] = new JMenuItem("删除(D)");
+		jmi[12] = new JMenuItem("查找(F)");
+		jmi[13] = new JMenuItem("查找下一个(N)");
+		jmi[14] = new JMenuItem("替换(R)");
+		jmi[15] = new JMenuItem("转到(G)");
+		jmi[16] = new JMenuItem("全选(A)");
+		jmi[16].addActionListener(this);
+		jmi[17] = new JMenuItem("日期/时间(D)");
+		//实现格式(O)菜单的菜单项组件
+		jmi[18] = new JMenuItem("自动换行(W)");
+		jmi[19] = new JMenuItem("字体(F)");
+		//实现格式(O)菜单的菜单项组件
+		jmi[20] = new JMenuItem("状态栏(S)");
+		//实现帮助(H)菜单的菜单项组件
+		jmi[21] = new JMenuItem("查看帮助(H)");
+		jmi[22] = new JMenuItem("关于记事本(A)");
+		//实现文本域组件
+		jta = new JTextArea();
+		jta.getDocument().addDocumentListener(new DocumentListener() {
+			   //监听文本内容的插入事件；
+			   public void removeUpdate(DocumentEvent e) {
+			    //keyword = textField.getText();
+				   flag = 1;
+			   }
+			   //监听文本内容的插入事件； 
+			   public void insertUpdate(DocumentEvent e) {
+			    //keyword = textField.getText();
+				   flag = 1;
+			   }
+			   //监听文本属性的变化； 
+			   public void changedUpdate(DocumentEvent e) {
+				   flag = 1;
+			   }
+		});
+		//向滚动窗格添加文本域组件
+	    jsp = new JScrollPane(jta);
+		//添加组件
+		//向文件菜单中添加菜单项
+		for(int i=0;i< 7;i++)
+		 jm[0].add(jmi[i]);
+		//向编辑菜单中添加菜单项
+		for(int i=7;i<=17;i++)
+		 jm[1].add(jmi[i]);
+		//向格式菜单中添加菜单项
+		for(int i=18;i<=19;i++)
+		 jm[2].add(jmi[i]);
+		//向查看菜单中添加菜单项
+		jm[3].add(jmi[20]);
+		//向帮助菜单中添加菜单项
+		for(int i=21;i<=22;i++)
+			jm[4].add(jmi[i]);
+		//向菜单栏中添加菜单
+		for(int i=0;i< 5;i++)
+			jmb.add(jm[i]);
+			
+		
+		//添加滚动窗格组件
+		this.add(jsp);
+		//设置菜单栏
+		this.setJMenuBar(jmb);
+		//设置窗体图标
+		//this.setIconImage((new ImageIcon("images/NotePadLogo.jpg")).getImage());
+		//设置窗体标题
+		this.setTitle("NotePad");
+    	//设置窗体大小
+		this.setSize(700,500);
+		//设置窗体起始位置
+		this.setLocation(400,150);
+		//设置窗体关闭方式
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//设置窗体可见
+		this.setVisible(true);
+   }
+
+	public void init2() {
+		JLabel ifSaveLabel = new JLabel("是否更改保存");
+		Panel head = new Panel();
+		Panel body = new Panel();
+		Panel footer = new Panel();
+		
+		head.add(ifSaveLabel);
+		footer.add(saveButton);
+		footer.add(notsaveButton);
+		footer.add(cancelButton);
+			
+		saveButton.addActionListener(this);
+		notsaveButton.addActionListener(this);
+		cancelButton.addActionListener(this);
+
+		alertFrame.setLayout(new BorderLayout());
+		alertFrame.add(head, BorderLayout.NORTH);
+		alertFrame.add(footer, BorderLayout.SOUTH);
+		
+		
+		alertFrame.setTitle("提示");
+		alertFrame.setSize(300,100);
+		alertFrame.setLocation(500, 200);
+		alertFrame.setDefaultCloseOperation(alertFrame.EXIT_ON_CLOSE);
+		alertFrame.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == jmi[0]) { //新建功能
+			NotePad notePadWindow = new NotePad();
+		}
+		if (e.getSource() == jmi[1]) { //打开功能
+			if (jta.getText().equals("")) {
+				try {
+					openMenuItemClick();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}else {
+				jta.setText(null);
+				try {
+					openMenuItemClick();
+					
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
+		if (e.getSource() == jmi[2]) { //保存功能
+			try {
+				saveMenuItemClick();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		} 
+		if (e.getSource() == jmi[3]) { //另存为功能
+			try {
+				asSaveMenuItemClick();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		if (e.getSource() == jmi[6]) { //退出功能
+			try {
+				exitMenuItemClick();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		if (e.getSource() == jmi[16]) {//保存功能
+			selectAll();
+		}
+		if (e.getSource() == saveButton) { //退出保存
+			try {
+				saveMenuItemClick();
+				System.exit(0);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		if (e.getSource() == notsaveButton) { //退出不保存
+			System.exit(0);
+		}
+		if(e.getSource() == cancelButton) { //取消退出
+			alertFrame.setVisible(false);
+		}
+	}	
+	
+	
+	//"打开"事件处理函数
+	private void openMenuItemClick() throws IOException {
+		//打开文件选择器，选择文件
+		//JFileChooser chooser = new JFileChooser();
+		//过滤扩展名
+		//创建文件名过滤器对象
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("txt","txt");
+		chooser.setFileFilter(filter);
+		//展示打开文件选择器
+		//返回用户的选择结果
+		int userChoose = chooser.showOpenDialog(this);
+			
+		//如果用户选择打开，则读取内容，否则返回
+		if(userChoose == JFileChooser.CANCEL_OPTION) {
+			return;
+		}
+		//读取文件内容
+		selectedFile = chooser.getSelectedFile();		
+		if(selectedFile!=null&&selectedFile.isFile()) {
+			//创建IO流对象
+			FileReader fr = new FileReader(selectedFile);	
+			//定义数组，记录本次读取到的字符内容
+			char[] chars = new char[1024];
+			//定义变量，记录本次读取到的字符个数
+			int len;
+			//读取内容
+			while((len=fr.read(chars))!=-1) {
+				//将本次读取到的内容转为字符串
+				String thisS = new String(chars,0,len);
+				//将内容添加到GUI的文本域组件中
+				jta.append(thisS);
+			}
+			//关闭流
+			fr.close();
+		}
+	}
+		
+	//"保存"事件处理函数
+	private void saveMenuItemClick() throws IOException{	
+		//判断成员位置是否已经存在File对象
+		if(selectedFile!=null) {
+			//获取文件内容
+			String data = jta.getText();
+			//写出文件内容
+			//创建IO流对象
+			FileWriter fw = new FileWriter(selectedFile);	
+			//写出内容
+			fw.write(data);
+			//关闭流
+			fw.close();
+		}else {//如果是新建的文件
+			//打开文件选择器，选择文件
+			//过滤扩展名
+			//创建文件名过滤器对象
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("txt","txt");
+			chooser.setFileFilter(filter);
+			
+			File newFile = new File("新建文本文件");
+			//为文件选择器设置默认文件名
+			chooser.setSelectedFile(newFile);
+			//展示   保存文件选择器
+			int userChoose = chooser.showSaveDialog(this);
+			//如果用户选择保存，则写出内容，否则返回
+			if(userChoose==JFileChooser.APPROVE_OPTION) {
+				//获取用户选择的文件
+				File selectedFile2 = chooser.getSelectedFile();
+				//去掉空白，转为小写判断用户选择的文件是否有.txt扩展名，如果没有加上.txt
+				if(!selectedFile2.getName().trim().toLowerCase().endsWith(".txt")) {
+					selectedFile2 = new File(selectedFile2.getParent(),selectedFile2.getName()+".txt");
+				}
+				//获取用户选择文件所在目录
+				File parentDIR = selectedFile2.getParentFile();
+				//遍历当前文件夹，判断是否有重名的文件
+				File[] listFiles = parentDIR.listFiles(); 
+				//依次获取文件夹中每一文件对象与保存的文件对象名称做对比 
+				for (File file : listFiles) {
+					if(file.getName().equals(selectedFile2.getName())) {
+						//提示有重复文件，是否覆盖
+						int isCover = JOptionPane.showConfirmDialog(this, "当前目录已经存在相同名称的文件，是否覆盖？");
+						if(isCover==JOptionPane.OK_OPTION) {
+							//获取文件内容   
+							String data = jta.getText();
+							//写出文件内容
+							//创建IO流对象
+							FileWriter fw = new FileWriter(selectedFile2);
+							//写出内容
+							fw.write(data);
+							//关闭流
+							fw.close();
+						}else {
+							return;
+						}
+					}
+				}
+				//如果没有重复文件，直接写出即可
+				//获取文件内容
+				String data = jta.getText();
+				//写出文件内容
+				//创建IO流对象
+				FileWriter fw = new FileWriter(selectedFile2);
+				//写出内容
+				fw.write(data);
+				//关闭流
+				fw.close();
+			}
+		}
+	}
+	//"另存为"事件处理函数
+	public void asSaveMenuItemClick() throws IOException {
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("txt","txt");
+		chooser.setFileFilter(filter);
+		
+		File newFile = new File("新建文本文件");
+		//为文件选择器设置默认文件名
+		chooser.setSelectedFile(newFile);
+		//展示   保存文件选择器
+		int userChoose = chooser.showSaveDialog(this);
+		//如果用户选择保存，则写出内容，否则返回
+		if(userChoose==JFileChooser.APPROVE_OPTION) {
+			//获取用户选择的文件
+			File selectedFile2 = chooser.getSelectedFile();
+			//去掉空白，转为小写判断用户选择的文件是否有.txt扩展名，如果没有加上.txt
+			if(!selectedFile2.getName().trim().toLowerCase().endsWith(".txt")) {
+				selectedFile2 = new File(selectedFile2.getParent(),selectedFile2.getName()+".txt");
+			}
+			//获取用户选择文件所在目录
+			File parentDIR = selectedFile2.getParentFile();
+			//遍历当前文件夹，判断是否有重名的文件
+			File[] listFiles = parentDIR.listFiles(); 
+			//依次获取文件夹中每一文件对象与保存的文件对象名称做对比 
+			for (File file : listFiles) {
+				if(file.getName().equals(selectedFile2.getName())) {
+					//提示有重复文件，是否覆盖
+					int isCover = JOptionPane.showConfirmDialog(this, "当前目录已经存在相同名称的文件，是否覆盖？");
+					if(isCover==JOptionPane.OK_OPTION) {
+						//获取文件内容   
+						String data = jta.getText();
+						//写出文件内容
+						//创建IO流对象
+						FileWriter fw = new FileWriter(selectedFile2);
+						//写出内容
+						fw.write(data);
+						//关闭流
+						fw.close();
+					}else {
+						return;						}
+				}
+			}
+				
+			//如果没有重复文件，直接写出即可
+			//获取文件内容
+			String data = jta.getText();
+			//写出文件内容
+			//创建IO流对象
+			FileWriter fw = new FileWriter(selectedFile2);
+			//写出内容
+			fw.write(data);
+			//关闭流
+			fw.close();
+		}
+	}
+	
+	//"全选"事件处理函数
+	public void selectAll() {
+		jta.selectAll();
+	}
+	
+		
+	//"退出"事件处理函数
+	public void exitMenuItemClick() throws IOException { 
+		if (flag == 0) { //文本内容未改变
+			System.exit(0);
+		}else if (flag == 1) {//文本内容改变，则提示是否保存
+			init2();
+		} 
+	}
+		
+	
+	public static void main(String[] args) {
+		//创建NotePad实例对象
+		new NotePad();
+	}
+}
